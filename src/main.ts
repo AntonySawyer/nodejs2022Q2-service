e4d54648848df0ca36c9b8c92c4af389957172d9
AntonySawyer <a.krutenok@gmail.com>
2023-02-03 11:10:45 +0300

    fix: Updated yaml doc and controllers prefix values for keep endpoints in same way as in tests; added useless import in artists.e2e.spec.ts into eslint ignore

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import * as dotenv from 'dotenv';
 
 import { AppModule } from './app.module';
 import { DEFAULT_APP_PORT } from './shared/server/defaultPort';
-import { GLOBAL_API_PREFIX } from './shared/server/globalPrefix';
 import { setupSwagger } from './shared/server/setupSwagger';
 
 dotenv.config();
@@ -14,8 +13,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   setupSwagger(app);
-
-  app.setGlobalPrefix(GLOBAL_API_PREFIX);
 
   await app.listen(PORT || DEFAULT_APP_PORT);
 }
