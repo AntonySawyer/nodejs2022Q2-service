3d48921001897a72027861b6b887c825ac5cb1a2
AntonySawyer <a.krutenok@gmail.com>
2023-02-15 09:53:54 +0300

    feat: Connect to DB from nest app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,9 +8,38 @@ import { TracksModule } from './tracks/tracks.module';
 import { AlbumsModule } from './albums/albums.module';
 import { ArtistsModule } from './artists/artists.module';
 import { FavsModule } from './favs/favs.module';
+import { UserEntity } from './users/entities/user.entity';
+import { ArtistEntity } from './artists/entities/artist.entity';
+import { AlbumEntity } from './albums/entities/album.entity';
+import { TrackEntity } from './tracks/entities/track.entity';
+import { FavEntity } from './favs/entities/fav.entity';
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+} = process.env;
 
 @Module({
-  imports: [UsersModule, TracksModule, AlbumsModule, ArtistsModule, FavsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: POSTGRES_HOST,
+      port: Number(POSTGRES_PORT),
+      username: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DB,
+      entities: [UserEntity, ArtistEntity, AlbumEntity, TrackEntity, FavEntity],
+      synchronize: true,
+    }),
+    UsersModule,
+    TracksModule,
+    AlbumsModule,
+    ArtistsModule,
+    FavsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
